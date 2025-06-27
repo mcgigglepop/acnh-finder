164e694225633f1a811d6532bf65497a0e8fc57e
cristopher ontiveros <christoph.alano@gmail.com>
2025-06-27 05:04:50 -0500

    fish and user fish tables

--- terraform/modules/dynamodb/main.tf
+++ terraform/modules/dynamodb/main.tf
@@ -12,3 +12,39 @@ resource "aws_dynamodb_table" "user_profiles" {
     Name = "UserProfiles"
   }
 }
+
+resource "aws_dynamodb_table" "fish" {
+  name           = "Fish"
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "fish_id"
+
+  attribute {
+    name = "fish_id"
+    type = "S"
+  }
+
+  tags = {
+    Name = "Fish"
+  }
+}
+
+resource "aws_dynamodb_table" "user_fish" {
+  name           = "UserFish"
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "user_id"
+  range_key      = "fish_id"
+
+  attribute {
+    name = "user_id"
+    type = "S"
+  }
+
+  attribute {
+    name = "fish_id"
+    type = "S"
+  }
+
+  tags = {
+    Name = "UserFish"
+  }
+}
