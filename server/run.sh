c2c3a77e0b4ba81f5d018e7a7ee976fafc0d5cfa
cristopher ontiveros <christoph.alano@gmail.com>
2025-06-27 02:41:49 -0500

    update run script

--- server/run.sh
+++ server/run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-go build -o acnh-finder-build cmd/web/*.go && ./acnh-finder-build  -cache=false -production=false -cognito-user-pool-id=1234 -cognito-client-id=1234
+go build -o acnh-finder-build cmd/web/*.go && ./acnh-finder-build  -cache=false -production=false -cognito-user-pool-id=us-east-1_X2nr3u6aE -cognito-client-id=5ah5not8osfrbmootsercpe0ns
