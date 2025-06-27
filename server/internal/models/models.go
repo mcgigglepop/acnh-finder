ce0833db98217f22be0bf78022e1b622a30dc111
cristopher ontiveros <christoph.alano@gmail.com>
2025-06-27 05:05:18 -0500

    added time range struct and append to fish struct

--- server/internal/models/models.go
+++ server/internal/models/models.go
@@ -6,19 +6,25 @@ type User struct {
 }
 
 type Fish struct {
-	Name        string   `dynamodbav:"name"`
-	Icon        string   `dynamodbav:"icon"`
-	SellPrice   int      `dynamodbav:"sell_price"`
-	ShadowSize  string   `dynamodbav:"shadow_size"`
-	ShadowIcon  string   `dynamodbav:"shadow_icon"`
-	Location    string   `dynamodbav:"location"`
-	TimeOfDay   []string `dynamodbav:"time_of_day"` // e.g. ["9AM", "4PM", "ALL_DAY"]
-	MonthsNorth []int    `dynamodbav:"months_north"` // 1–12
-	MonthsSouth []int    `dynamodbav:"months_south"` // 1–12
+	FishID      string      `dynamodbav:"fish_id"`
+	Name        string      `dynamodbav:"name"`
+	Icon        string      `dynamodbav:"icon"`
+	SellPrice   int         `dynamodbav:"sell_price"`
+	ShadowSize  string      `dynamodbav:"shadow_size"`
+	ShadowIcon  string      `dynamodbav:"shadow_icon"`
+	Location    string      `dynamodbav:"location"`
+	TimeRanges  []TimeRange `dynamodbav:"time_ranges"`
+	MonthsNorth []int       `dynamodbav:"months_north"`
+	MonthsSouth []int       `dynamodbav:"months_south"`
 }
 
 type UserFish struct {
 	UserID string `dynamodbav:"user_id"`
 	FishID string `dynamodbav:"fish_id"`
 	Owned  bool   `dynamodbav:"owned"`
+}
+
+type TimeRange struct {
+	Start string `dynamodbav:"start"` // e.g. "16:00"
+	End   string `dynamodbav:"end"`   // e.g. "09:00"
 }
